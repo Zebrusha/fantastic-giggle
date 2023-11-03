a824ab991631a1fbcad12cf18a141fa18af7d24d
Anastasia Voronchikhina <zebrushaa@mail.ru>
2023-11-03 19:46:27 +0300

    add ispr

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int balance = 200;
         int cashback = 0;
         int replenishment = 400;
 
         balance = balance + replenishment;
-        if (replenishment > 1000){
-            cashback = replenishment/100;
-        }
-        else{
+        if (replenishment > 1000) {
+            cashback = replenishment / 100;
+        } else {
             System.out.println("Баланс: " + balance);
             System.out.println("Сумма пополнения: " + replenishment);
             System.out.println("Бонусные рубли начисляються при пополнении более 1000 рублей");
